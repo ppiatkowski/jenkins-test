f68ca576cd7ffa8332acf63a2cbb5950ee0de15a
Paweł Piątkowski <pawel.piatkowski@mobica.com>
2018-12-21 14:11:25 +0100

    Init

---
+++ JenkinsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsTest
+//
+//  Created by Paweł Piątkowski on 17/12/2018.
+//  Copyright © 2018 Paweł Piątkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
